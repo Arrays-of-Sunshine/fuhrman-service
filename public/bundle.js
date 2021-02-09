1fc6ecaf4a5c35dd526ca811503c35831a0eb123
Molly Fuhrman <mollyfuhrman@gmail.com>
2021-02-08 16:58:23 -0800

    Getting new relic and k6 working, tried to get loader.io to work.

--- public/bundle.js
+++ public/bundle.js
@@ -71,7 +71,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _App_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./App.jsx */ \"./client/App.jsx\");\n\n\n // require(\"babel-polyfill\");\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_App_jsx__WEBPACK_IMPORTED_MODULE_2__.default, null), document.getElementById('product_images'));\n\n//# sourceURL=webpack:///./client/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n/* harmony import */ var react_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n/* harmony import */ var _App_jsx__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./App.jsx */ \"./client/App.jsx\");\n\n\n // require(\"babel-polyfill\");\n// require('newrelic');\n\nreact_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement(_App_jsx__WEBPACK_IMPORTED_MODULE_2__.default, null), document.getElementById('product_images'));\n\n//# sourceURL=webpack:///./client/index.js?");
 
 /***/ }),
 
--- server/index-sv.js
+++ server/index-sv.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // const db = require('../database/index-db.js').connection;
 const db = require('../database/index-mongo-db.js');
+require('newrelic');
+// const loader = require('./loaderio.txt');
 
 const app = express();
 // const cors = require('cors');
@@ -16,6 +18,7 @@ app.use(express.urlencoded());
 app.use(express.static('public'));
 app.use('/', express.static('public'));
 app.use('/bundle', express.static('public/bundle.js'));
+app.use('/loaderio-9cf1d2f5faa5b640d46d74b1d3640633.txt', express.static('public/loaderio-9cf1d2f5faa5b640d46d74b1d3640633.txt'));
 
 app.get('/products/:id', (req, res) => {
   let { id } = req.params;
